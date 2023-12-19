34a4d8a58c44916c510a659e2183d8eaec922428
rafaover <rafamsilva@gmail.com>
2023-12-19 13:36:57 +1100

    TopBar to use with Scaffold.

---
+++ app/src/main/java/com/feeltheboard/amphibians/ui/components/AmphiTopBar.kt
@@ -0,0 +1,23 @@
+package com.feeltheboard.amphibians.ui.components
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.feeltheboard.amphibians.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmphibianTopBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+        },
+    )
+}
