376c8cd7a4748195d9bbee91cc4c9cd8e32e74a1
rafaover <rafamsilva@gmail.com>
2024-02-06 15:31:42 +1100

    Implemented all retrofit service. - Converted the data collected to string using converter scalar. - Used a coroutine in a interface to get the data. - Used a singleton(object) to acquire the data.

--- app/src/main/java/com/feeltheboard/amphibians/network/AmphiApiService.kt
+++ app/src/main/java/com/feeltheboard/amphibians/network/AmphiApiService.kt
@@ -1,8 +1,25 @@
 package com.feeltheboard.amphibians.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
-interface AmphiApiService {
-    @GET("amphibians")
-    suspend fun getAmphibiansData(): List<Amphibians>
+
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
+private val retrofit= Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface AmphibiansApiService {
+    @GET("amphibians") //retrofit endpoint
+    suspend fun getAmphibiansData(): String
+}
+
+object AmphibiansApi {
+    /** "lazy initialization" is when object creation is purposely delayed, until you actually need
+    that object, to avoid unnecessary computation or use of other computing resources. */
+    val retrofitService : AmphibiansApiService by lazy {
+        retrofit.create(AmphibiansApiService::class.java) }
 }
--- app/src/main/java/com/feeltheboard/amphibians/network/AmphiData.kt
+++ app/src/main/java/com/feeltheboard/amphibians/network/AmphiData.kt
@@ -1,13 +1,10 @@
 package com.feeltheboard.amphibians.network
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Amphibians(
-    val name: String,
-    val type: String,
-    val description: String,
-    @SerialName("img_src")
-    val imgSrc: String
-)
+//@Serializable
+//data class Amphibians(
+//    val name: String,
+//    val type: String,
+//    val description: String,
+//    @SerialName("img_src")
+//    val imgSrc: String
+//)
