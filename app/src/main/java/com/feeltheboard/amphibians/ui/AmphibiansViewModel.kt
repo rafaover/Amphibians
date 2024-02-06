70b416edfd3d32f72cd3e176a74472452bfdc3e4
rafaover <rafamsilva@gmail.com>
2024-02-06 15:57:01 +1100

    Implemented when statement to act in case of different outcome on error handling. - Added loading/error composables in case of not successfull load.

--- app/src/main/java/com/feeltheboard/amphibians/ui/AmphibiansViewModel.kt
+++ app/src/main/java/com/feeltheboard/amphibians/ui/AmphibiansViewModel.kt
@@ -7,9 +7,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.feeltheboard.amphibians.network.AmphibiansApi
 import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface AmphibiansUiState {
+    data class Success(val data: String): AmphibiansUiState
+    object Loading: AmphibiansUiState
+    object Error: AmphibiansUiState
+}
 
 class AmphibiansViewModel: ViewModel() {
-    var amphibiansUiState: String by mutableStateOf("")
+    var amphibiansUiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
         private set
 
     init {
@@ -17,9 +24,14 @@ class AmphibiansViewModel: ViewModel() {
     }
 
     private fun getAmphibiansData() {
-        viewModelScope.launch {
-            val listResult = AmphibiansApi.retrofitService.getAmphibiansData()
-            amphibiansUiState = listResult
+        try {
+            viewModelScope.launch {
+                val listResult = AmphibiansApi.retrofitService.getAmphibiansData()
+                amphibiansUiState = AmphibiansUiState.Success(listResult)
+            }
+        }
+        catch (e: IOException) {
+            amphibiansUiState = AmphibiansUiState.Error
         }
     }
 }
--- app/src/main/java/com/feeltheboard/amphibians/ui/components/HomeScreen.kt
+++ app/src/main/java/com/feeltheboard/amphibians/ui/components/HomeScreen.kt
@@ -1,21 +1,37 @@
 package com.feeltheboard.amphibians.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.feeltheboard.amphibians.R
+import com.feeltheboard.amphibians.ui.AmphibiansUiState
 
 @Composable
 fun HomeScreen(
-    amphibiansUiState: String,
+    amphibiansUiState: AmphibiansUiState,
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp)
 ) {
-    ResultScreen(amphibiansUiState, modifier.padding(top = contentPadding.calculateTopPadding()))
+    when(amphibiansUiState) {
+        is AmphibiansUiState.Success -> ResultScreen(
+            amphibiansUiState.data,
+            modifier.padding(top = contentPadding.calculateTopPadding())
+        )
+        is AmphibiansUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+        is AmphibiansUiState.Error -> ErrorScreen(modifier = modifier.fillMaxSize())
+    }
 }
 
 @Composable
@@ -25,5 +41,32 @@ fun ResultScreen(amphibians: String, modifier: Modifier = Modifier) {
         modifier = modifier
     ) {
         Text(text = amphibians)
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
